9362129b41e32966b7b908f2bd0a2beb6355f479
mashingan <rahmat.d.ruffy@gmail.com>
2017-09-25 06:14:50 +0700

    change java dependency lib to kotlin's

--- client.kt
+++ client.kt
@@ -2,21 +2,32 @@ package reactor.client
 
 import java.net.Socket
 import java.net.UnknownHostException
-import java.io.*
+//import java.io.*
+import java.io.OutputStreamWriter
+import java.io.IOException
+
+fun prompt(): String {
+    print("> ")
+    return readLine() ?: ""
+}
+
+fun OutputStreamWriter.println(msg: String) {
+    write(msg, 0, msg.length)
+    flush()
+}
 
 fun main(args: Array<String>) {
     val ip = "127.0.0.1"
     val port = 3000
     val socket = Socket(ip, port)
-    val outstream = PrintWriter(socket.getOutputStream(), true)
-    val instream = BufferedReader(InputStreamReader(
-            socket.getInputStream()))
-    val stdin = BufferedReader(InputStreamReader(System.`in`))
+    val outstream = socket.getOutputStream().writer()
+    val instream = socket.getInputStream().bufferedReader()
     println("Client connected to host: $ip port $port")
     println("Type \"bye\" to quit")
     println("Tell what your name to the server...")
 
-    var userinput = stdin.readLine() ?:
+    print("> ")
+    var userinput = readLine() ?:
         throw IOException("Cannot read from console")
     while (true) {
         if (userinput.isEmpty() || userinput == "bye") {
@@ -25,10 +36,9 @@ fun main(args: Array<String>) {
         outstream.println(userinput)
         println("Sending the input: $userinput")
         println("The server says: ${instream.readLine()}")
-        userinput = stdin.readLine() ?: ""
+        userinput = prompt()
     }
     outstream.close()
     instream.close()
-    stdin.close()
     socket.close()
 }
